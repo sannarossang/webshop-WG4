e220ea6e9fa4ff53b756d6b5d24c57f24f6e04ae
Julia Carlberg <juliacarlbrg@gmail.com>
2022-12-22 15:50:41 +0100

    added cart function

--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -22,3 +22,39 @@ export function getCart() {
 
 function confirm() {}
 function checkOut() {}
+
+export function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  const { target } = event;
+  if (target) (target as HTMLButtonElement).matches(".dropbtn");
+  var myDropdown = document.getElementById("myDropdown");
+  if (myDropdown.classList.contains("show")) {
+    myDropdown.classList.remove("show");
+  }
+};
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("cartButton");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+function closeSpan() {
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+}
+
+closeSpan();
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
