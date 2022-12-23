4ebc4900134368e9233c774480a1dee057b612cf
Julia Carlberg <juliacarlbrg@gmail.com>
2022-12-23 15:31:55 +0100

    added modal

--- src/ts/productdetails.ts
+++ src/ts/productdetails.ts
@@ -80,3 +80,34 @@ function backaWindow() {
 }
 
 createHTMLForItem(products);
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("cartButton");
+let closeBtn = document.getElementById("closeButton");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+let burgerBtn = document.getElementById("burgerMenu");
+let burgerModal = document.getElementById("burgerModal");
+
+burgerBtn.onclick = function () {
+  burgerModal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == burgerModal) {
+    burgerModal.style.display = "none";
+  }
+};
