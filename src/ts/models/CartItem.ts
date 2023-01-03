16efe38a560782d8f69a0f0f7eeffc8713fd4cef
sannarossang <sanna.rossang@gmail.com>
2023-01-03 09:38:50 +0100

    updated class CartItem

--- src/ts/models/CartItem.ts
+++ src/ts/models/CartItem.ts
@@ -1,13 +1,9 @@
 import { Product } from "./Products";
 
 export class CartItem {
-  product: Product;
-  quantity: number;
-  totalPrice: number;
-
-  constructor(product: Product, quantity: number, totalPrice: number) {
-    this.product = product;
-    this.quantity = quantity;
-    this.totalPrice = totalPrice;
-  }
+  constructor(
+    public product: Product,
+    public quantity: number,
+    public totalPrice: number
+  ) {}
 }
